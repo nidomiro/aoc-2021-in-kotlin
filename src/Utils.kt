e881e41e7f686349852e5987595f1a6f87398ff6
Niclas <niclas@nidomiro.de>
2021-11-30 12:00:38 +0100

    feat: restructures template code

--- src/Utils.kt
+++ src/Utils.kt
@@ -5,9 +5,13 @@ import java.security.MessageDigest
 /**
  * Reads lines from the given input txt file.
  */
-fun readInput(name: String) = File("src", "$name.txt").readLines()
+fun readInputFromRoot(name: String) = File("src", name).readLines()
+fun readInput(folder: String, name: String) = File("src/${folder}", name).readLines()
+fun bindReadInput(folder: String): (String) -> List<String> = {name -> readInput(folder, name)}
+
 
 /**
  * Converts string to md5 hash.
  */
 fun String.md5(): String = BigInteger(1, MessageDigest.getInstance("MD5").digest(toByteArray())).toString(16)
+
--- src/day01/main.kt
+++ src/day01/main.kt
@@ -1,3 +1,9 @@
+package day01
+
+import bindReadInput
+
+val readInput = bindReadInput("day01")
+
 fun main() {
     fun part1(input: List<String>): Int {
         return input.size
@@ -8,10 +14,10 @@ fun main() {
     }
 
     // test if implementation meets criteria from the description, like:
-    val testInput = readInput("Day01_test")
+    val testInput = readInput("output_test.txt")
     check(part1(testInput) == 1)
 
-    val input = readInput("Day01")
+    val input = readInput("input.txt")
     println(part1(input))
     println(part2(input))
 }
