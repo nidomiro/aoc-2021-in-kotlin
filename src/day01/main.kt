1bd70d5c634da8d1809c19441a83f24218c42543
Niclas <niclas@nidomiro.de>
2021-12-01 17:50:07 +0100

    refactor(day01): use sequences; extract increasingValues counter

--- src/day01/main.kt
+++ src/day01/main.kt
@@ -8,9 +8,8 @@ fun main() {
     val input = readInput("input.txt")
     val depths = input.map { it.toInt()}
 
-    fun part1(input: List<Int>): Int {
-
-        return input.windowed(2).sumOf {
+    fun Sequence<Int>.increasingValuesCount(): Int {
+        return windowed(2).sumOf {
             val (last, current) = it
             val increase = current - last
             if (increase > 0){
@@ -19,28 +18,19 @@ fun main() {
                 0
             }
         }
+    }
 
+    fun part1(input: List<Int>): Int {
+        return input.asSequence().increasingValuesCount()
     }
 
     val part1Result = part1(depths)
     println("Part1: $part1Result")
 
-
-
     fun part2(input: List<Int>): Int {
-        val valueWindows = input.windowed(3)
+        val valueWindows = input.asSequence().windowed(3)
         val windowSums = valueWindows.map { it.sum() }
-        val diffWindows = windowSums.windowed(2)
-
-        return diffWindows.sumOf {
-            val (last, current) = it
-            val increase = current - last
-            if (increase > 0){
-                1 as Int
-            } else {
-                0
-            }
-        }
+        return windowSums.increasingValuesCount()
     }
 
     val part2Result = part2(depths)
