67162286cc0394d6b996dc197de2546b2b2c6605
Niclas <niclas@nidomiro.de>
2021-12-01 12:09:46 +0100

    refactor(day01): part1

--- src/day01/main.kt
+++ src/day01/main.kt
@@ -4,39 +4,27 @@ import bindReadInput
 
 val readInput = bindReadInput("day01")
 
-enum class Difference{
-    NA,
-    INCREASED,
-    DECREASED
-}
-
 fun main() {
     val input = readInput("input.txt")
     val depths = input.map { it.toInt()}
 
     fun part1(input: List<Int>): Int {
 
-        var lastValue = input.first()
-        val differences = depths.map {
-            val currentValue = it
-            val increase = currentValue - lastValue
-            //println("$lastValue -> $currentValue = $increase")
-            lastValue = currentValue
-
-            if(increase == 0) {
-                Difference.NA
-            } else if (increase > 0){
-                Difference.INCREASED
+        return input.windowed(2).flatMap {
+            val (last, current) = it
+            val increase = current - last
+            if (increase > 0){
+                listOf(1)
             } else {
-                Difference.DECREASED
+                listOf()
             }
-        }
+        }.count()
 
-        return differences.count { it == Difference.INCREASED }
     }
 
     val part1Result = part1(depths)
     println("Part1: $part1Result")
+
 
 
 }
