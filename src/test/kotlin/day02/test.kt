6eb8940593d0ca2007b025f28876583adc52d0c4
Niclas <niclas@nidomiro.de>
2021-12-02 11:35:54 +0100

    refactor(day02): refactors tests

--- src/test/kotlin/day02/test.kt
+++ src/test/kotlin/day02/test.kt
@@ -3,42 +3,29 @@ package day02
 import io.kotest.core.spec.style.DescribeSpec
 import io.kotest.matchers.shouldBe
 
-class MyTests : DescribeSpec({
+class Tests : DescribeSpec({
+    val input = listOf(
+        "forward 5",
+        "down 5",
+        "forward 8",
+        "up 3",
+        "down 8",
+        "forward 2"
+    )
+
     describe("part1") {
         it("fulfills the given example") {
-            val input = listOf(
-                "forward 5",
-                "down 5",
-                "forward 8",
-                "up 3",
-                "down 8",
-                "forward 2"
-            )
-
             val result = part1(input)
-
             result shouldBe (15 to 10)
         }
-
     }
 
     describe("part2") {
         it("fulfills the given example") {
-            val input = listOf(
-                "forward 5",
-                "down 5",
-                "forward 8",
-                "up 3",
-                "down 8",
-                "forward 2"
-            )
-
             val result = part2(input)
-
             result.x shouldBe 15
             result.depth shouldBe 60
         }
-
     }
 
 
