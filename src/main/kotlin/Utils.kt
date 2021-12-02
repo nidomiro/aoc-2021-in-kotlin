598e7c87e662da46a6fbcd7af7b90b051a9e6e8f
Niclas <niclas@nidomiro.de>
2021-12-02 10:26:10 +0100

    fix: readInput folder definition

--- src/main/kotlin/Utils.kt
+++ src/main/kotlin/Utils.kt
@@ -6,7 +6,7 @@ import java.security.MessageDigest
  * Reads lines from the given input txt file.
  */
 fun readInputFromRoot(name: String) = File("src", name).readLines()
-fun readInput(folder: String, name: String) = File("src/${folder}", name).readLines()
+fun readInput(folder: String, name: String) = File("src/main/kotlin/${folder}", name).readLines()
 fun bindReadInput(folder: String): (String) -> List<String> = { name -> readInput(folder, name) }
 
 
